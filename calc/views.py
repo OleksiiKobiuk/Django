41af0093a741f02edc38d075103b53e4bb702525
OleksiiKobiuk <al_kob@ukr.net>
2021-07-23 00:47:37 +0300

    Calculyator done

--- calc/views.py
+++ calc/views.py
@@ -1,33 +1,25 @@
 from django.shortcuts import render
 
-# a = 'HI'
+
 err = False
 
 
 def calculation(request, num1, sign, num2):
+    a = None
 
-        if sign == '+':
-            a = num1 + num2
-        elif sign == '-':
-            a = num1 - num2
-        elif sign == 'dev':
+    if sign == '+':
+        a = num1 + num2
+    elif sign == '-':
+        a = num1 - num2
+    elif sign == 'dev':
+        try:
             a = num1 / num2
-        elif sign == '*':
-            a = num1 * num2
+        except ZeroDivisionError:
+            pass
+        sign = ':'
+    elif sign == '*':
+        a = num1 * num2
 
 
-        # try:
-        #     if sign == '+':
-        #         a = num1 + num2
-        #     elif sign == '-':
-        #         a = num1 - num2
-        #     elif sign == 'dev':
-        #         a = num1 / num2
-        #     elif sign == '*':
-        #         a = num1 * num2
-        # except:
-        #     a = 'wrong'
-
-        # return render(request, 'index_calc.html', {'num1': num1}, {'sign': sign}, {'num2': num2},
-        #           {'result': a}, {'err': err})
-        return render(request, 'index_calc.html',{'result': a})
+    return render(request, 'index_calc.html', {'num1': num1,'sign': sign, 'num2': num2,'result': a})
+        # return render(request, 'index_calc.html',{'result': a})
