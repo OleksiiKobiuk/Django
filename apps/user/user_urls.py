edd1cc0e0e0ec748e8bbf994ae7daeb3a78af035
OleksiiKobiuk <al_kob@ukr.net>
2021-08-31 00:14:45 +0300

    Lesson 5_customUser_oneToOne_oneToMany_RetrUpdDelUser

--- apps/user/user_urls.py
+++ apps/user/user_urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import UserListCreateView
+from .views import UserListCreateView, UserRetrieveUpdateDelete
 
 urlpatterns = [
-    path('', UserListCreateView.as_view(), name = 'user_list_create')
+    path('', UserListCreateView.as_view(), name = 'user_list_create'),
+    # виведення/видалення/оновлення 1 юзера
+    path('/<int:user_id>', UserRetrieveUpdateDelete.as_view(), name = 'retrieve_user')
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -8,3 +8,9 @@ UserModel: User = get_user_model()
 class UserListCreateView(ListCreateAPIView):
     serializer_class = UserSerializer
     queryset = UserModel.objects.all()
+
+# виведення/видалення/оновлення юзера по id
+class UserRetrieveUpdateDelete(RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializer
+    lookup_url_kwarg = 'user_id'
+    queryset = UserModel.objects.all()
